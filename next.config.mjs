1887b42870c88fb544d9539700dc55548867ebec
DeveshChaturvedi <deveshchaturvedi2003@gmail.com>
2025-07-09 13:48:21 +0530

    Portal Test Dep-3

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    transpilePackages: ['react-pdf'],
+  transpilePackages: ['react-pdf'],
+
   webpack(config, { isServer }) {
-    // Support loading PDF worker
+    if (!isServer) {
+      // Avoid bundling server-only packages in client
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        canvas: false,
+        fs: false,
+        path: false,
+        stream: false,
+      };
+    }
+
+    // Allow loading PDF worker
     config.module.rules.push({
       test: /pdf\.worker(\.min)?\.js$/,
       use: { loader: 'file-loader' },
